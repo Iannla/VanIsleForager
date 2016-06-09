86df788d55ede7ba02c099d2ac11a78c6aaac56d
Helen King <Heiskanen@Petris-MacBook-Pro.local>
2016-06-09 07:57:42 -0700

    Updated june 8
    script in index updated, map.js key commented out,

--- map.js
+++ map.js
@@ -3,32 +3,34 @@ var firebase = new Firebase("https://project-6896618617155316776.firebaseio.com/
 
   // Set the configuration for your app
   // TODO: Replace with your project's config object
-  var config = {
+ /* var config = {
             apiKey: "AIzaSyB7RJBuDs5KU-iK-wSssDLU0J36edRXHdQ",
             authDomain: "project-6896618617155316776.firebaseapp.com",
             databaseURL: "https://project-6896618617155316776.firebaseio.com",
             storageBucket: "project-6896618617155316776.appspot.com",
           };
           firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  var database = firebase.database();
-
-
-
-
+*/
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.455, lng: -123.375},
-    zoom: 12
+    zoom: 12,
+    styles: [{
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+    }, {
+      featureType: 'transit.station',
+      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+    }],
+    disableDoubleClickZoom: true
   });
 
-  // Add marker on user click
-  map.addListener('click', function(e) {
-    firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
-  });
+// Add marker on user click
+map.addListener('click', function(e) {
+  firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
+});
 
-  // Create a heatmap.
+// Create a heatmap.
   var heatmap = new google.maps.visualization.HeatmapLayer({
     data: [],
     map: map,
@@ -43,7 +45,7 @@ function initMap() {
     // A LatLng object literal (as above) could be used, but the heatmap
     // in the next step requires a google.maps.LatLng object.
     var latLng = new google.maps.LatLng(newPosition.lat, newPosition.lng);
-
-    heatmap.getData().push(latLng);
+ 
+     heatmap.getData().push(latLng);
   });
 }
